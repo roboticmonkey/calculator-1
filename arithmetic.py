091070b135ba4dacbd183963a02150fc2de10b52
Hackbright Student <--system>
2016-04-06 12:31:28 -0700

    added the rest of functions

--- arithmetic.py
+++ arithmetic.py
@@ -6,24 +6,35 @@ def add(num1, num2):
 	return num1 + num2
 
 def subtract(num1, num2):
-	""" This takes two number and subtracts them.
+	""" This takes two numbers and returns the difference of them.
 	"""
 	return num1 - num2
 
 def multiply(num1, num2):
-    pass
+    """ This takes two numbers and returns the result of multiply them.
+    """
+    return num1 * num2
 
 def divide(num1, num2):
-    pass
+    """ This takes two numbers and divides them and returns the total.
+    """
+    return num1 / num2
 
 def square(num1):
-    pass
+    """ This take a number and returns it's square
+    """
+    return num1 * num1
 
 def cube(num1):
-    pass
+    """ This take a number and returns it's cube
+    """
+    return num1*num1*num1
 
 def power(num1, num2):
-    pass
+    """ This returns the first number raised to the power of the second number."""
+    return num1 ** num2
 
 def mod(num1, num2):
-    pass
+    """ Returns the remaider of first number divided by second number.
+    """
+    return num1 % num2
